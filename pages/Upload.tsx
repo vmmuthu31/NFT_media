3b3a49239454429099a8e45ff6e8987c5994766c
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-25 10:49:48 +0530

    updated the upload page  with post section

--- pages/Upload.tsx
+++ pages/Upload.tsx
@@ -64,24 +64,6 @@ export default function Upload() {
               </div>
 
               <div className="grid grid-cols-3 gap-6">
-                <div className="col-span-3 sm:col-span-2">
-                  <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
-                    Username
-                  </label>
-                  <div className="mt-1 rounded-md shadow-sm flex">
-                    <span className="bg-gray-50 border border-r-0 border-gray-300 rounded-l-md px-3 inline-flex items-center text-gray-500 sm:text-sm">
-                      workcation.com/
-                    </span>
-                    <input
-                      type="text"
-                      name="username"
-                      id="username"
-                      autoComplete="username"
-                      className="focus:ring-indigo-500 focus:border-indigo-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
-                    />
-                  </div>
-                </div>
-
                 <div className="col-span-3">
                   <label htmlFor="about" className="block text-sm font-medium text-gray-700">
                     About
