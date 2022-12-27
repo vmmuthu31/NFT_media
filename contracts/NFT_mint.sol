2df25c5537689c48e3dd51376eff7e776454cec3
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-27 23:38:24 +0530

    added the code for minting of nft in contract and added contractjs connect with frontend

---
+++ contracts/NFT_mint.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MINTNFT is ERC721URIStorage { 
+
+    using Counters for Counters.Counter; 
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Factory NFT", "FTN") {
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
