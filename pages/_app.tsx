41555fc1a2b283e9117508f9a4dc25cc266e3f5b
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-28 17:49:30 +0530

    updated the content page with rainbow wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import "@biconomy/web3-auth/dist/src/style.css"
 import "@rainbow-me/rainbowkit/styles.css"
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,10 @@ import { Suspense } from "react";
 
 
 import styles from '../styles/Home.module.css'
-// import Content from './Content'
-import "@biconomy/web3-auth/dist/src/style.css"
 import Hero from './Hero';
 
 
 export default function Home() {
-  const SocialLoginDynamic = dynamic(
-    () => import("./Content").then((res) => res.default),
-    {
-      ssr: false,
-    }
-  );
-
   return (
     <div className={styles.container}>
       <Head>
@@ -26,15 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      <div>
-      
-          <Suspense fallback={<div>Loading...</div>}>
-          
-            <SocialLoginDynamic />
-          
-          </Suspense>
-        
-      </div>
+      <Hero/>
       </main>
     </div>
   )
