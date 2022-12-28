843aaf1eb532064b80c3f0514facd6aa932be418
Vairamuthu <mvairamuthu2003@gmail.com>
2022-12-28 15:46:58 +0530

    updated the wagmi wallet to the Hero page

--- pages/Header.js
+++ pages/Header.js
@@ -6,6 +6,7 @@ import { HiXMark } from "react-icons/hi2";
 import truncateEthAddress from "truncate-eth-address";
 
 import Link from "next/link";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -16,91 +17,6 @@ const navigation = [
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const [provider, setProvider] = useState();
-  const [account, setAccount] = useState();
-  const [smartAccount, setSmartAccount] = useState(null);
-  const [scwAddress, setScwAddress] = useState("");
-  const [scwLoading, setScwLoading] = useState(false);
-  const [socialLoginSDK, setSocialLoginSDK] = useState(null);
-  const connectWeb3 = useCallback(async () => {
-    if (typeof window === "undefined") return;
-    console.log("socialLoginSDK", socialLoginSDK);
-    if (socialLoginSDK?.provider) {
-      const web3Provider = new ethers.providers.Web3Provider(
-        socialLoginSDK.provider
-      );
-      setProvider(web3Provider);
-      const accounts = await web3Provider.listAccounts();
-      setAccount(accounts[0]);
-      return;
-    }
-    if (socialLoginSDK) {
-      socialLoginSDK.showWallet();
-      return socialLoginSDK;
-    }
-    const sdk = new SocialLogin();
-    await sdk.init(ethers.utils.hexValue(80001));
-    setSocialLoginSDK(sdk);
-    sdk.showConnectModal();
-    sdk.showWallet();
-    return socialLoginSDK;
-  }, [socialLoginSDK]);
-
-  // if wallet already connected close widget
-  useEffect(() => {
-    console.log("hidelwallet");
-    if (socialLoginSDK && socialLoginSDK.provider) {
-      socialLoginSDK.hideWallet();
-    }
-  }, [account, socialLoginSDK]);
-
-  // after metamask login -> get provider event
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      if (account) {
-        clearInterval(interval);
-      }
-      if (socialLoginSDK?.provider && !account) {
-        connectWeb3();
-      }
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [account, connectWeb3, socialLoginSDK]);
-
-  const disconnectWeb3 = async () => {
-    if (!socialLoginSDK || !socialLoginSDK.web3auth) {
-      console.error("Web3Modal not initialized.");
-      return;
-    }
-    await socialLoginSDK.logout();
-    socialLoginSDK.hideWallet();
-    setProvider(undefined);
-    setAccount(undefined);
-    setScwAddress("");
-  };
-
-  useEffect(() => {
-    async function setupSmartAccount() {
-      setScwAddress("");
-      setScwLoading(true);
-      const smartAccount = new SmartAccount(provider, {
-        activeNetworkId: ChainId.GOERLI,
-        supportedNetworksIds: [ChainId.GOERLI],
-      });
-      await smartAccount.init();
-      const context = smartAccount.getSmartAccountContext();
-      setScwAddress(context.baseWallet.getAddress());
-      setSmartAccount(smartAccount);
-      setScwLoading(false);
-    }
-    if (!!provider && !!account) {
-      setupSmartAccount();
-      console.log("Provider...", provider);
-    }
-  }, [account, provider]);
-
   return (
     <div className="bgimg">
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
@@ -169,18 +85,8 @@ export default function Header() {
                   </Link>
                 ))}
               </div>
-              <div className="text-white py-3 px-3 ">
-                {account && <div>Address: {truncateEthAddress(account)}</div>}
-              </div>
-              {account ? (
-                <button className="btn-grad1 px-2 " onClick={disconnectWeb3}>
-                  Disconnect Wallet
-                </button>
-              ) : (
-                <button className="btn-grad px-2" onClick={connectWeb3}>
-                  Connect Wallet
-                </button>
-              )}
+
+              <ConnectButton />
             </div>
           </nav>
           <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
